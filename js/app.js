488426c490e48eeb57f6b58e4459ce2a6e830d41
WebDeveloperApolo <rakibodesk7@gmail.com>
2022-03-01 21:03:31 +0600

    Modal added

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ const searchInput = document.getElementById("search-input");
 const errorBody = document.getElementById("error-body");
 const errorTxt = document.getElementById("error-txt");
 const notFoundTxt = document.getElementById("not-found");
+const phoneRow = document.getElementById("phone-row");
+const details = document.getElementById("details");
+const modal = document.getElementById("modal");
 
 // Alert messages
 const alerts = (errMsg) => {
@@ -31,16 +34,66 @@ const displayPhones = (phones) => {
     notFoundTxt.classList.remove("d-none");
   } else {
     phones.forEach((phone) => {
-      console.log(phone.length);
+      const col = document.createElement("div");
+      col.classList.add("col-12");
+      col.classList.add("col-lg-4");
+      col.classList.add("g-4");
+      col.classList.add("shadow-lg");
+      col.classList.add("p-3");
+      col.classList.add("bg-body");
+      col.classList.add("rounded");
+
+      col.innerHTML = `<div class="card card-hight">
+      <img
+        src="${phone.image}"
+        class="card-img-top"
+        alt="..."
+      />
+      <div class="card-body container-fluied">
+        <h4><b>Phone Name:</b> ${phone.phone_name}</h4>
+        <h5><b>Phone Brand:</b> ${phone.brand}</h5>
+        <button
+          class="btn custom-btn text-white "
+          onclick="loadDetails('${phone.slug}')"
+          >See details</button>
+      </div>
+    </div>`;
+      phoneRow.appendChild(col);
     });
   }
 };
 
 // Load details data
 const loadDetails = (id) => {
-  console.log(id);
+  fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+    .then((res) => res.json())
+    .then((data) => displayDetails(data.data));
 };
 // Display phones details
-const displayDetails = () => {
-  console.log("Phone details");
+const displayDetails = (detailsInfo) => {
+  details.innerHTML = `<button
+  class="btn btn-danger text-white position-absolute top-0 mt-2 me-3 end-0 p-2"
+  type="button" onclick="modalClose()"
+>
+  X
+</button>
+<div class="mt-5 d-flex justify-content-center">
+  <img
+    src="${detailsInfo.image}"
+    class="card-img-top w-25"
+    alt="..."
+  />
+</div>
+<div class="card-body text-center">
+  <h4><b>Phone name:</b> ${detailsInfo.name}</h4>
+  <h5><b>Phone brand:</b> ${detailsInfo.brand}</h5>
+  <h5><b>Releash date:</b> ${detailsInfo.releaseDate}</h5>
+  <h5><b>Sensors:</b>${detailsInfo.mainFeatures.sensors} </h5>
+</div>`;
+  modal.classList.remove("d-none");
+  details.classList.remove("d-none");
+};
+// Modal close
+const modalClose = () => {
+  modal.classList.add("d-none");
 };
